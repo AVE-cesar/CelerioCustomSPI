94cf63f8d4ba475b4f8647c881de54cb15e3b770
AVE-cesar <bertrand.Ave@gmail.com>
2017-11-14 09:30:55 +0100

    14 November 2017: junit test

--- src/test/java/ave/bertrand/celerio/spi/ExtendedEntityTest.java
+++ src/test/java/ave/bertrand/celerio/spi/ExtendedEntityTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.jaxio.celerio.configuration.database.JdbcType;
 import com.jaxio.celerio.configuration.entity.ColumnConfig;
 import com.jaxio.celerio.configuration.entity.EntityConfig;
+import com.jaxio.celerio.factory.RelationCollisionUtil;
 import com.jaxio.celerio.model.Attribute;
 import com.jaxio.celerio.model.Entity;
 
@@ -13,22 +15,35 @@ public class ExtendedEntityTest {
 
 	@Test
 	public void testGetAttributesList() {
-//		ExtendedEntity extEntity = new ExtendedEntity();
-//		
-//		Entity entity = new Entity();
-//		EntityConfig entityConfig = new EntityConfig();
-//		entityConfig.setEntityName("test");
-//		
-//		Attribute attribute = new Attribute();
-//		ColumnConfig columnConfig = new ColumnConfig();
-//		columnConfig.setLabel("test");
-//		attribute.setColumnConfig(columnConfig);
-//		attribute.setEntity(entity);
-//		entity.addAttribute(attribute);
-//		
-//		extEntity.init(entity);
-//		
-//		assertEquals(extEntity.getAttributesList(), "");
+		// on instancie la classe à tester
+		ExtendedEntity extEntity = new ExtendedEntity();
+		
+		RelationCollisionUtil relationUtil = new RelationCollisionUtil();
+		
+		// on crée et configure une classe Entity (=TABLE)
+		Entity entity = new Entity();
+		EntityConfig entityConfig = new EntityConfig();
+		entityConfig.setEntityName("test");
+		entity.setEntityConfig(entityConfig);
+		System.out.println(entity.getCollisionUtil());
+		
+		// on crée et configure une classe Attribute (=COLONNE)
+		Attribute attribute = new Attribute();
+		ColumnConfig columnConfig = new ColumnConfig();
+		columnConfig.setType(JdbcType.VARCHAR);
+		columnConfig.setLabel("test");
+		attribute.setColumnConfig(columnConfig);
+		attribute.setEntity(entity);
+		
+		System.out.println(attribute);
+		
+		entity.addAttribute(attribute);
+		
+		extEntity.init(entity);
+		
+		System.out.println("nb colonnes: " + extEntity.getAttributesList());
+		
+		assertEquals(extEntity.getAttributesList(), "");
 	}
 
 }
